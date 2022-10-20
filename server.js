7d0972d225ab0bb809cef5617818fee1dcc8ca08
Carlos Eduardo Teixeira Zulian <71992281+CaduZulian@users.noreply.github.com>
2022-10-20 15:54:19 -0300

    add chatList channel

--- server.js
+++ server.js
@@ -9,27 +9,69 @@ app.use(cors());
 
 var clients = [];
 
-io.on("connection", function(client) {
+const chatList = [
+  {
+    chat_name: "TESTE EMPRESA WEBSOCKET S/A",
+    cnpj: "12345678911234",
+    date: "2022-10-19T17:52:08.808Z",
+    has_unread_messages: "3",
+    reference_number: "7777777-60.2022.9.27.0001",
+  },
+  {
+    chat_name: "TESTE EMPRESA WEBSOCKET S/A",
+    cnpj: "12345678911234",
+    date: "2022-10-19T17:52:08.808Z",
+    has_unread_messages: "2",
+    reference_number: "7777777-60.2022.9.27.0002",
+  },
+  {
+    chat_name: "TESTE EMPRESA WEBSOCKET S/A",
+    cnpj: "12345678911234",
+    date: "2022-10-19T17:52:08.808Z",
+    has_unread_messages: "1",
+    reference_number: "7777777-60.2022.9.27.0003",
+  },
+  {
+    chat_name: "TESTE EMPRESA WEBSOCKET S/A",
+    cnpj: "12345678911234",
+    date: "2022-10-19T17:52:08.808Z",
+    has_unread_messages: "1",
+    reference_number: "7777777-60.2022.9.27.0004",
+  },
+  {
+    chat_name: "TESTE EMPRESA WEBSOCKET S/A",
+    cnpj: "12345678911234",
+    date: "2022-10-19T17:52:08.808Z",
+    has_unread_messages: "0",
+    reference_number: "7777777-60.2022.9.27.0005",
+  },
+]
+
+io.on("connection", function (client) {
   console.log('user connected: ' + client.id)
 
   if (clients[client.id]) {
-    clients.push({id: client.id});
+    clients.push({ id: client.id });
   }
-  
+
+  client.on('chatList', () => {
+    io.emit('chatList', chatList)
+  })
+
   client.on('joinRoom', (room) => {
     client.join(room)
-    clients[client.id] = {...clients[client.id], room: room};
+    clients[client.id] = { ...clients[client.id], room: room };
 
     console.log('user ' + client.id + ' joined to room ' + room)
   })
 
   client.on('leaveRoom', () => {
-    if(clients[client.id].room) {
+    if (clients[client.id].room) {
       client.leave(clients[client.id].room)
-  
+
       console.log('user ' + client.id + ' leaved of room ' + clients[client.id].room)
 
-      clients[client.id] = {id: client.id};
+      clients[client.id] = { id: client.id };
     }
   })
 
@@ -47,10 +89,10 @@ io.on("connection", function(client) {
     })
   })
 
-  client.on("disconnect", function() {
+  client.on("disconnect", function () {
     console.log("client " + client.id + " disconnected")
-      clients = clients.filter(({id}) => id !== client.id);
-      console.log(client.rooms);
+    clients = clients.filter(({ id }) => id !== client.id);
+    console.log(client.rooms);
   });
 });
 
