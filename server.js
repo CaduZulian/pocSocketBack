00c8c9ea6a157a4010f619ef1cc1a1bda07b555e
Carlos Eduardo Teixeira Zulian <71992281+CaduZulian@users.noreply.github.com>
2022-10-20 14:21:56 -0300

    update server.js

--- server.js
+++ server.js
@@ -1,26 +1,59 @@
 const express = require("express");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-require('dotenv').config();
+const app = express();
+const port = 8002;
+var server = require("http").Server(app);
+const io = require("socket.io")(server);
+const cors = require("cors");
 
-const app = require('./app');
-const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+app.use(cors());
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.emit('message', 'a user connected')
+var clients = [];
 
-  socket.on('message', (message) => {
-    console.log(message)
-    socket.emit('message', message)
+io.on("connection", function(client) {
+  console.log('user connected: ' + client.id)
+
+  if (clients[client.id]) {
+    clients.push({id: client.id});
+  }
+  
+  client.on('joinRoom', (room) => {
+    client.join(room)
+    clients[client.id] = {...clients[client.id], room: room};
+
+    console.log('user ' + client.id + ' joined to room ' + room)
   })
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  client.on('leaveRoom', () => {
+    if(clients[client.id].room) {
+      client.leave(clients[client.id].room)
+  
+      console.log('user ' + client.id + ' leaved of room ' + clients[client.id].room)
+
+      clients[client.id] = {id: client.id};
+    }
+  })
+
+  client.on('sendMessage', (message) => {
+    console.log('user ' + client.id + ' send a message in room ' + clients[client.id].room + ': ' + message.message)
+
+    console.log(client.rooms);
+
+    io.in(clients[client.id].room).emit('chatMessage', {
+      id: 11111,
+      message: message.message,
+      sender_document: message.sender_document,
+      created_at: new Date(),
+      updated_at: new Date()
+    })
+  })
+
+  client.on("disconnect", function() {
+    console.log("client " + client.id + " disconnected")
+      clients = clients.filter(({id}) => id !== client.id);
+      console.log(client.rooms);
   });
 });
 
-console.log(`\nserver running at port http://localhost:${process.env.PORT}\n`)
-
-httpServer.listen(process.env.PORT);
+server.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
